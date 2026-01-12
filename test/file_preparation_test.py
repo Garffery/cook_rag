46f4c35f635ea92966375a9165798c4e398fe9e9
garffy <13226133323@163.com>
2026-01-12 20:21:11 +0800

    代码同步

---
+++ test/file_preparation_test.py
@@ -0,0 +1,17 @@
+import logging
+
+from rag_module import DataPreparationModule
+
+
+# 配置日志
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+dataPath = "../data/cook/"
+
+preparationer = DataPreparationModule(dataPath)
+print(preparationer)
+res = preparationer.load_documents()
+# print(res)
